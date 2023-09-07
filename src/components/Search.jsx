b4c54e1c5caa2b4c7098a2611532b3da548a3027
Bekzat <esimhanuli13@gmail.com>
2023-09-07 18:18:01 +0600

    loading component

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,20 +1,33 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import search from '../assets/icon-search.svg'
+import WordInfo from './WordInfo';
+import Loading from './Loading';
 
 function Search() {
     const [word, setWord] = useState("");
     const [result, setResult] = useState(null);
 
-    async function searchForWord(word){
+    const inpRef = useRef(null);
+    const btnRef = useRef(null);
+
+    const handleKeyPress = (e) =>{
+        if(e.key === "Enter"){
+            btnRef.current.click();
+        }
+    }
+    
+
+    const searchForWord = async (word) =>{
 
         try {
             const response = await fetch(import.meta.env.VITE_API + word);
             if (!response.ok) {
               throw new Error('Network response was not ok');
             }
-            const result = await response.json();
-            setResult(result);
-            console.log(result);
+            const data = await response.json();
+            setResult(data[0]);
+            inpRef.current.value = null;
+            console.log(result)
           } catch (error) {
             console.error('Error fetching data:', error);
         }        
@@ -23,17 +36,17 @@ function Search() {
   return (
     <div className='w-full mt-12'>
         <div className="relative">
-                <input className="block w-full p-4 pl-10 text-xl text-gray-900 border border-gray-300 rounded-xl bg-gray-50" placeholder="Search for any word..." required onChange={(e)=>setWord(e.target.value)}/>
-                <button type="submit" className="text-white absolute right-2.5 bottom-2.5" onClick={()=>searchForWord(word)}>
+                <input ref={inpRef} className="font-bold block w-full p-4 pl-10 text-xl text-gray-900 border border-gray-300 rounded-xl bg-gray-50" placeholder="Search for any word..." required onChange={(e)=>setWord(e.target.value)} onKeyPress={handleKeyPress}/>
+                <button ref={btnRef} type="submit" className="text-white absolute right-2.5 bottom-2.5" onClick={()=>searchForWord(word)}>
                    <img src={search} alt="search icon" /> 
                 </button>
         </div>
 
         {
-            result && (
-                <div>
-                   
-                </div>
+            result ? (
+                  <WordInfo result={result}/>
+            ) : (
+              <Loading/>
             )
         }
     </div>
---
+++ src/components/WordInfo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function WordInfo(result) {
+  return (
+    <div>
+        {/* {result.word} */}
+    </div>
+  )
+}
+
+export default WordInfo
